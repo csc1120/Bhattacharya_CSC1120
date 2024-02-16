a86e14b96522fa26c5e84e6e531ec3f111b6adbf
Saikath Bhattacharya <saikath1@users.noreply.github.com>
2024-02-16 13:41:31 -0600

    week 5 codes  changed

---
+++ CSC1120_Code/src/Week5/People.java
@@ -0,0 +1,15 @@
+package Week5.csc1120;
+
+public class People {
+    private String name;
+    private int age;
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
