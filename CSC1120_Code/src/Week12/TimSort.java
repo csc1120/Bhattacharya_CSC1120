d9c20f3fefa0482b54a29886a8e47db9e7c754c9
Saikath Bhattacharya <saikath1@users.noreply.github.com>
2024-04-18 15:56:52 -0500

    sorting algorithms

--- CSC1120_Code/src/Week12/TimSort.java
+++ CSC1120_Code/src/Week12/TimSort.java
@@ -1,89 +1,84 @@
 package Week12;
 
 /**
- *     Step 1 - Divide the array into the number of blocks known as run.
- *     Step 2 - Consider the size of run, either 32 or 64.
- *     Step 3 - Sort the individual elements of every run one by one using insertion sort.
- *     Step 4 - Merge the sorted runs one by one using the merge function of merge sort.
- *     Step 5 - Double the size of merged sub-arrays after every iteration.
+ * Step 1 -- divide the array into number of blocks called run
+ * Step 2-- run 32 or 64
+ * Step 3-- sort the individual elements of every run using insertion sort
+ * Step 4-- merge the sorted runs one by one
+ * Step 5-- double the size of the merged sub-array after iteration
  */
 public class TimSort {
-    public static final int MIN_RUN = 32;
+    public static int MIN_RUN= 32;
+
     public static void main(String[] args) {
-        int[] arr = { 12, 11, 13, 5, 6, 7,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
-        int n = arr.length;
-        System.out.println("The array : ;");
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
+        int arr[] ={12,11,13,5,6,1};
         timSort(arr);
-        System.out.println("\nThe sorted array : ;");
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
-        System.out.println();
+        printArray(arr);
     }
-
-    private static void timSort(int[] arr) {
-
-        int n = arr.length;
-        for (int i = 0; i < n; i += MIN_RUN) {
-            insertionSort(arr, i, Math.min((i + MIN_RUN-1), (n - 1)));
+    private  static  void  timSort(int[] arr){
+        int n= arr.length;
+        for(int i=0; i<n; i+=MIN_RUN){
+            insertionSort(arr);
         }
-        for (int size = MIN_RUN; size < n; size = 2 * size) {
-            // Start merging from size MIN_RUN (or 32). It will merge to 64, 128, 256 and so on
-            for (int left = 0; left < n; left += 2 * size) {
-                int mid = left + size - 1;
-                int right = Math.min((left + 2 * size - 1), (n - 1));
-                merge(arr, left, mid, right);
+        for (int size= MIN_RUN; size<n; size= 2*size){
+            // start merging from size min_run
+            for(int left =0; left<n; left+=2*size){
+                int mid = left+size-1;
+                int right= Math.min((left+2*size-1),(n-1));
+                merge(arr, left, right);
             }
         }
     }
-
-
-    private static void insertionSort(int[] arr, int i, int min) {
-        for (int j = i + 1; j <= min; j++) {
-            int key = arr[j];
-            int k = j - 1;
-            while (k >= i && arr[k] > key) {
-                arr[k + 1] = arr[k];
-                k--;
+    static  void  insertionSort(int arr[]){
+        int len= arr.length, temp, j;
+        for(int i = 1; i < len; i++){
+            temp =arr[i];
+            for(j=i; j>0; j--) {
+                if (arr[j - 1] > temp) {
+                    arr[j] = arr[j - 1];
+                }
+                else {
+                    break;
+                }
             }
-            arr[k + 1] = key;
+            arr[j] = temp;
         }
     }
-    private static void merge(int[] arr, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-        for (int i = 0; i < n1; i++) {
-            L[i] = arr[left + i];
-        }
-        for (int j = 0; j < n2; j++) {
-            R[j] = arr[mid + 1 + j];
-        }
-        int i = 0, j = 0;
-        int k = left;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++;
+    private  static  void merge(int arr[], int lower, int upper){
+        int m = (lower+ upper)/2;
+        int a[] = new int[m- lower+1]; // left array
+        int b[] = new int[upper- m]; // right array
+        int i, k=0, k1= 0, k2= 0;  // k1 and k2 are indices of the left and right arrays
+        // copy the element to the left side
+        for(i =lower; i<= m; i++, k++)
+            a[k] = arr[i];
+        k=0;
+        // copy elements to the right side
+        for(; i<= upper; i++, k++)
+            b[k] = arr[i];
+        // merge the left and right array
+        for(i= lower; i<= lower && k1< m-lower+1 && k2< upper-m; i++ ){
+            if (a[k1] < b[k2]) {
+                arr[i] = a[k1];
+                k1++;
             } else {
-                arr[k] = R[j];
-                j++;
+                arr[i] = b[k2];
+                k2++;
             }
-            k++;
         }
-        while (i < n1) {
-            arr[k] = L[i];
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        for (; k1 < m - lower + 1; k1++) // copying the remaining elements of the left array
+            arr[i++] = a[k1];
+        for (; k2 < upper - m; k2++)
+            arr[i++] = b[k2];
+    }
+    public static void printArray(int arr[]){
+        int n= arr.length;
+        for(int i= 0; i< n ; ++i){
+            System.out.print(arr[i]+ " " );
+            System.out.println();
         }
     }
+
 
 
 }
