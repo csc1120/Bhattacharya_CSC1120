4215470c3f4436d19cffcb2345114ad8393a7338
Saikath Bhattacharya <saikath1@users.noreply.github.com>
2024-04-19 10:55:47 -0500

    sorting algorithms

---
+++ CSC1120_Code/src/Week14/SBBST.java
@@ -0,0 +1,33 @@
+package Week14;
+
+
+// An AVL Tree node
+
+    // AVL Tree implementation
+
+    public class SBBST {
+        public static void main(String[] args) {
+            BinarySearchTree bst = new BinarySearchTree();
+            AVLTree avl = new AVLTree();
+            // unbalanced keys
+           // int[] keys = {10, 20, 30, 40, 50, 60};
+            int[] keys = {5,9, 6,15,11,16,10,13};
+
+            for (int key : keys) {
+                bst.insert(key);
+                avl.insert(key);
+            }
+
+            System.out.println("Height of BST: " + bst.height());
+            System.out.println("Height of AVL: " + avl.height());
+            System.out.printf("AVL balance factor: %d%n", avl.getBalance(avl.root))   ;
+
+            System.out.println("Binary Search Tree:");
+            bst.printTree();
+            System.out.printf("%n ------------------------------------ %n");
+            System.out.println("AVL Tree:");
+            avl.printTree();
+           // avl.printTree(avl.root, 0);
+        }
+    }
+
